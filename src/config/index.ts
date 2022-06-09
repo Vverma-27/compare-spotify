5299677bece90b6370486141e2360ee47dfeaded
Vihaan <vverma270705@gmail.com>
2022-06-10 00:15:45 +0530

    App completed

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,10 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "8ba59f9588b74c729fa1a97e777f4499";
-export const redirectUri = "http://localhost:3000";
+export const redirectUri =
+  process.env.NODE_ENV === "production"
+    ? "https://compare-spotify.vercel.app/"
+    : "http://localhost:3000";
 export const scopes = [
   "user-top-read",
   // "user-read-currently-playing",
